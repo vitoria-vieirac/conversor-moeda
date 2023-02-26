cc165fe402f8f6733bdd5d42c2043b1cf12046a4
vitoria-vieirac <114622524+vitoria-vieirac@users.noreply.github.com>
2023-02-26 18:48:30 -0300

    Primeiro Commit

---
+++ Projeto Convers/scripts.js
@@ -0,0 +1,51 @@
+const button = document.getElementById("convert-button")
+const select = document.getElementById("currency-select")
+
+const dolar = 5.2
+const euro = 5.9
+
+const convertValues = () => {
+    const inputReais = document.getElementById("input-real").value
+    const realValueText = document.getElementById("real-value-text")
+    const currencyValueText = document.getElementById("currency-value-text")
+
+    realValueText.innerHTML = new Intl.NumberFormat("pt-BR" , {
+        style: "currency",
+        currency: "BRL",
+    }).format(inputReais)
+
+
+    if(select.value === "US$ Dólar Americano") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("en-US" , {
+        style: "currency",
+        currency: "USD",
+    }).format(inputReais/dolar)}
+
+    if(select.value === "€ Euro") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("de-DE" , {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputReais/euro)
+
+    }
+
+    
+}
+
+changeCurrency = () => {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImg = document.getElementById("currency-img")
+
+    if(select.value === "US$ Dólar Americano") {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = "./eua.png"
+    }
+
+    if(select.value === "€ Euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./euro.png"
+    }
+}
+
+button.addEventListener("click", convertValues)
+select.addEventListener("change", changeCurrency)
